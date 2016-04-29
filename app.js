9f2e5dae24a5fa29268e09dd4bb687806b1b4bd6
Trevor Nowalk <trevor.nowalk@twcable.com>
2016-04-29 09:45:14 -0400

    Initial boilerplate

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const logger = require('./lib/logger')();
+const redact = require('redact-object');
+const Bot = require('./lib/bot');
+const Config = require('./lib/config');
+
+require('./lib/utils');
+
+let config;
+
+/**
+ * Load config
+ */
+const rawConfig = (() => {
+  let retVal;
+  try {
+    retVal = require('./config');
+  } catch (exception) {
+    retVal = require('./config.default');
+  }
+
+  return retVal;
+})();
+
+try {
+  config = Config.parse(rawConfig);
+} catch (error) {
+  logger.error('Could not parse config', error);
+  process.exit(1);
+}
+
+logger.info('Using the following configuration:', redact(config, ['token', 'pass']));
+
+const bot = new Bot(config);
+bot.start();
